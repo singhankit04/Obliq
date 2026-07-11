113b61abc0ed523cc2e2fcaf0bf4bc729c6ec976
Ankit Kumar <ankitmth2000@gmail.com>
2026-07-12 01:48:23 +0530

    added enviornmental variable

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ,
   credentials: true // Important for cookies!
 }));
 
